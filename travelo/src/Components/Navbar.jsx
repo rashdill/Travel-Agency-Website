0349f1dc26b7a858d49ea5c9e013647d27c84404
Rashmi Herath <diliherath1018@gmail.com>
2024-04-26 14:33:33 +0530

    main functions

--- travelo/src/Components/Navbar.jsx
+++ travelo/src/Components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
     <div>
       
     </div>
+    
   )
 }
