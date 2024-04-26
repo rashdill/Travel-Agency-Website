28164ebb30cfea57fe04a0c2abf41d5272880659
Rashmi Herath <diliherath1018@gmail.com>
2024-04-26 19:32:44 +0530

    nav bar completed

--- travelo/src/Components/Navbar.jsx
+++ travelo/src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
                 <img src={logo} alt="" />
                 Travelo
             </div>
-            <div className="Toggle">
+            <div className="toggle">
             </div>
         </div>
         <ul>
@@ -26,5 +26,63 @@ export default function Navbar() {
 
   )
 }
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display:flex;
+  justify-content: space-between;
+  align-items:center;
+  .brand{
+    .container{
+      cursor: pointer;pointer;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      gap:0.4rem;
+      font-size:1.2rem;
+      font-weight:900;
+      text-transform:uppercase;
+    }
+    .toggle{
+      display:none;
+
+    }
+  }
+  ul{
+    display:flex;
+    list-style-type:none;
+    gap:1rem;
+    li{
+      a{
+        text-decoration:none;
+        color:blue;
+        font-size:1.2rem;
+        transition: 0ms.1s ease-in-out;
+        &:hover{
+          color:#023e8a;
+        }
+      }
+      &:first-of-type{
+        a{
+          color:#023e8a;
+          font-weight:900;
+        }
+      }
+    }
+  }
+  button{
+    padding:0.5rem 1rem;
+    cursor:pointer;
+    border-radius:1rem;
+    border:none;
+    color:white;
+    background-color:#48cae4;
+    text-transform:uppercase;
+    font-size:1.1rem;
+    letter-spacing:0.1rem;
+    transition:0.3 ease-in-out;
+    &:hover{
+      background-color:#023e8a;
+    }
+
+  }
+`;
 
