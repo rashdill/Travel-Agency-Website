59bc35d7a9b6d2d0b362e0f5acadfe7752002e11
Rashmi Herath <diliherath1018@gmail.com>
2024-04-26 15:25:24 +0530

    add basic home,services,recommendation,tesetimonials

--- travelo/src/Components/Navbar.jsx
+++ travelo/src/Components/Navbar.jsx
@@ -4,9 +4,27 @@ import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <div>
-      
-    </div>
-    
+    <>
+      <Nav>
+        <div className="brand">
+            <div className="container">
+                <img src={logo} alt="" />
+                Travelo
+            </div>
+            <div className="Toggle">
+            </div>
+        </div>
+        <ul>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#services'>Services</a></li>
+            <li><a href='#recommendation'>Recommendation</a></li>
+            <li><a href='#tsetimonials'>Testimonials</a></li>
+        </ul>
+        <button>Connect</button>
+      </Nav>
+    </>
+
   )
 }
+const Nav = Styled.nav;
+
