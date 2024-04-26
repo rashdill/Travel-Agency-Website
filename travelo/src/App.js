b95fdbf8723ca1dd5f6794b08d6172f820da641e
Rashmi Herath <diliherath1018@gmail.com>
2024-04-26 14:25:47 +0530

    after link all parts

--- travelo/src/App.js
+++ travelo/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Services from './Components/Services'
+import Recommend from './Components/Recommend'
+import Testimonials from './Components/Testimonials'
+import Footer from './Components/Footer'
+import SrollToTop from './Components/SrollToTop'
+
 
 export default function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <SrollToTop/>
+    <Navbar/>
+    <Hero/>
+    <Services/>
+    <Recommend/>
+    <Testimonials/>
+    <Footer/>
+    
+      
+    </>
   )
 }
