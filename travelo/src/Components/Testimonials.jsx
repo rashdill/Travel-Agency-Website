2ecd09349ecfa034fca00c2bc60e8b1bcb704df4
Rashmi Herath <diliherath1018@gmail.com>
2024-04-27 12:29:33 +0530

    testimonials full code

--- travelo/src/Components/Testimonials.jsx
+++ travelo/src/Components/Testimonials.jsx
@@ -1,52 +1,106 @@
-import React from 'react'
-import StyleSheet from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import avatarImage from "../assets/avatarImage.jpeg";
-import styled from 'styled-components';
-
-
+import avatarImage1 from "../assets/avatarImage1.jpeg";
+import avatarImage2 from "../assets/avatarImage2.jpeg";
 export default function Testimonials() {
   return (
-    <Section>
+    <Section id="testimonials">
       <div className="title">
-        <h2>Happy Customer</h2>
+        <h2>Happy Customers</h2>
       </div>
       <div className="testimonials">
-        <p>
-          I am happy about this service..
-        </p>
-        <div className="info">
-          <img src={avatarImage} alt="" />
-          <div className="details">
-            <h4>Suneth Perera</h4>
-            <span>CEO - </span>
-          </div>  
+        <div className="testimonial">
+          <p>
+            Nice trip. Ireally enjoy it.
+          </p>
+          <div className="info">
+            <img src={avatarImage} alt="" />
+            <div className="details">
+              <h4>Mice steward</h4>
+              <span>CEO - abc Campany</span>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="testimonials">
-        <p>
-          I am happy about this service..
-        </p>
-        <div className="info">
-          <img src={avatarImage} alt="" />
-          <div className="details">
-            <h4>Suneth Perera</h4>
-            <span>CEO - </span>
-          </div>  
+        <div className="testimonial">
+          <p>
+            This is a nice service.I really happy.
+          </p>
+          <div className="info">
+            <img src={avatarImage1} alt="" />
+            <div className="details">
+              <h4>david mechel</h4>
+              <span>CEO - Shashaan Web Solutions</span>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="testimonials">
-        <p>
-          I am happy about this service..
-        </p>
-        <div className="info">
-          <img src={avatarImage} alt="" />
-          <div className="details">
-            <h4>Suneth Perera</h4>
-            <span>CEO - </span>
-          </div>  
+        <div className="testimonial">
+          <p>
+           I am happy about services
+          </p>
+          <div className="info">
+            <img src={avatarImage2} alt="" />
+            <div className="details">
+              <h4>Eric Frew</h4>
+              <span>CEO - Shashaan Web Solutions</span>
+            </div>
+          </div>
         </div>
       </div>
     </Section>
-  )
+  );
 }
-const Section = styled.section``;
+
+const Section = styled.section`
+  margin: 5rem 0;
+  .title {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  .testimonials {
+    display: flex;
+    justify-content: center;
+    margin: 0 2rem;
+    gap: 2rem;
+    .testimonial {
+      background-color: aliceblue;
+      padding: 2rem;
+      border-radius: 0.5rem;
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        transform: translateX(0.4rem) translateY(-1rem);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      }
+      .info {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+        align-items: center;
+        margin-top: 1rem;
+        img {
+          border-radius: 3rem;
+          height: 3rem;
+        }
+        .details {
+          span {
+            font-size: 0.9rem;
+          }
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 768px) {
+    .testimonials {
+      flex-direction: column;
+      margin: 0;
+      .testimonial {
+        justify-content: center;
+        .info {
+          flex-direction: column;
+          justify-content: center;
+        }
+      }
+    }
+  }
+`;
