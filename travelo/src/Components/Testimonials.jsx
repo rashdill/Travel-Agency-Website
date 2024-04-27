85733a936a0090a68d57125b1a20ab193a28d9cf
Rashmi Herath <diliherath1018@gmail.com>
2024-04-27 12:05:00 +0530

    basic image structure

--- travelo/src/Components/Testimonials.jsx
+++ travelo/src/Components/Testimonials.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import StyleSheet from 'styled-components';
+import avatarImage from "../assets/avatarImage.jpeg";
+import styled from 'styled-components';
+
 
 export default function Testimonials() {
   return (
-    <div>
-      
-    </div>
+    <Section>
+      <div className="title">
+        <h2>Happy Customer</h2>
+      </div>
+      <div className="testimonials">
+        <p>
+          I am happy about this service..
+        </p>
+        <div className="info">
+          <img src={avatarImage} alt="" />
+          <div className="details">
+            <h4>Suneth Perera</h4>
+            <span>CEO - </span>
+          </div>  
+        </div>
+      </div>
+      <div className="testimonials">
+        <p>
+          I am happy about this service..
+        </p>
+        <div className="info">
+          <img src={avatarImage} alt="" />
+          <div className="details">
+            <h4>Suneth Perera</h4>
+            <span>CEO - </span>
+          </div>  
+        </div>
+      </div>
+      <div className="testimonials">
+        <p>
+          I am happy about this service..
+        </p>
+        <div className="info">
+          <img src={avatarImage} alt="" />
+          <div className="details">
+            <h4>Suneth Perera</h4>
+            <span>CEO - </span>
+          </div>  
+        </div>
+      </div>
+    </Section>
   )
 }
+const Section = styled.section``;
