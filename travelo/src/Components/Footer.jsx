6dacbcfaa7a2b1ba524b5356cba8bbf235240f90
Rashmi Herath <diliherath1018@gmail.com>
2024-04-27 13:11:21 +0530

    final one

--- travelo/src/Components/Footer.jsx
+++ travelo/src/Components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <span>Copyright &copy 2024 Travelo.All rights reserved</span>
       
       <ul className="Links">
-            <li><a href='#home'>Home</a></li>
+            <li><a href='#hero'>Home</a></li>
             <li><a href='#services'>Services</a></li>
             <li><a href='#recommend'>Recommendation</a></li>
             <li><a href='#testimonials'>Testimonials</a></li>
