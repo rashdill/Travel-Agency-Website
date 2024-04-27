8922835866902a53794a664d4d132cc6cb97f8df
Rashmi Herath <diliherath1018@gmail.com>
2024-04-27 12:57:51 +0530

    scoial media icons add and link other pages

--- travelo/src/Components/Footer.jsx
+++ travelo/src/Components/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+import {BsLinkedin,BsFacebook} from "react-icons/bs";
+import {AiFillInstagram} from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <div>
+    <FooterContainer>
+      <span>Copyright &copy 2024 Travelo.All rights reserved</span>
       
-    </div>
+      <ul className="Links">
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#services'>Services</a></li>
+            <li><a href='#recommend'>Recommendation</a></li>
+            <li><a href='#testimonials'>Testimonials</a></li>
+      
+      </ul>
+      <ul className="social_links">
+        <li><BsFacebook/></li>
+        <li><BsLinkedin/></li>
+        <li><AiFillInstagram/></li>
+        
+        
+      </ul>
+    </FooterContainer>
   )
 }
+const FooterContainer = styled.footer``;
