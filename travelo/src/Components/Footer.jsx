0ce634075a022eb4aa496e698fe6f94119691312
Rashmi Herath <diliherath1018@gmail.com>
2024-04-27 13:08:37 +0530

    after adding css files

--- travelo/src/Components/Footer.jsx
+++ travelo/src/Components/Footer.jsx
@@ -25,4 +25,34 @@ export default function Footer() {
     </FooterContainer>
   )
 }
-const FooterContainer = styled.footer``;
+const FooterContainer = styled.footer`
+display:flex;
+justify-content:space-evenly;
+background-color:#d0d8ff;
+padding:2.5rem;
+border-radius:0.5rem;
+ul{
+  display:flex;
+  list-style-type:none;
+  gap:2rem;
+  li{
+    a{
+      text-decoration:none;
+      color:black;
+      transition:0.3s ease-in-out;
+      &:hover{
+        color:#302ce9;
+      }
+    }
+    svg{
+      font-size:1.3rem;
+      transition:0.3s ease-in-out;
+      &:hover{
+        color:#302ce9;
+      }
+
+
+    }
+  }
+}
+`;
