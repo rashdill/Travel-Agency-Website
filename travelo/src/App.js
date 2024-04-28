4af7617b72cfeb3f97096cb05ccb2ae72c188cc6
Rashmi Herath <diliherath1018@gmail.com>
2024-04-28 12:15:14 +0530

    final change

--- travelo/src/App.js
+++ travelo/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import Services from './Components/Services'
 import Recommend from './Components/Recommend'
 import Testimonials from './Components/Testimonials'
 import Footer from './Components/Footer'
-import SrollToTop from './Components/SrollToTop'
+import SrollToTop from './Components/ScrollToTop'
+import scrollreveal from "scrollreveal";
 
 
 export default function App() {
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "top",
+      distance: "80px",
+      duration: 2000,
+      reset: true,
+    });
+    sr.reveal(
+      `
+        nav,
+        #hero,
+        #services,
+        #recommend,
+        #testimonials,
+        footer
+        `,
+      {
+        opacity: 0,
+        interval: 300,
+      }
+    );
+  }, []);
   return (
     <>
     <SrollToTop/>
