8ad76c44b82a72f75f3867275ef61ccd8f4b113f
Rashmi Herath <diliherath1018@gmail.com>
2024-04-28 09:33:17 +0530

    after adding menue icon

--- travelo/src/Components/Navbar.jsx
+++ travelo/src/Components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from "styled-components";
 import logo from "../assets/logo.png";
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {VscChromeClose} from 'react-icons/vsc'
+
 
 export default function Navbar() {
+  const [navbarState,setNavbarState] = useState(false);
   return (
     <>
       <Nav>
@@ -12,6 +16,9 @@ export default function Navbar() {
                 Travelo
             </div>
             <div className="toggle">
+              {
+                navbarState ? <VscChromeClose onClick={()=> setNavbarState(false)}/> :<GiHamburgerMenu onClick={()=> setNavbarState(true)}/>
+              }
             </div>
         </div>
         <ul>
@@ -97,8 +104,9 @@ const Nav = styled.nav`
     }
     ul,button{
       display:none;
-      
+
     }
+    
   }
 `;
 
