31698627b1d5c9cdaf3508912c450c3809f9fc00
Rashmi Herath <diliherath1018@gmail.com>
2024-04-28 09:07:16 +0530

    after adjust screen (correct way)

--- travelo/src/Components/Hero.jsx
+++ travelo/src/Components/Hero.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import homeImage from "../assets/hero.png";
-
-
 export default function Hero() {
   return (
     <Section id="hero">
@@ -11,161 +9,154 @@ export default function Hero() {
       </div>
       <div className="content">
         <div className="title">
-          <h1>Travel To Explore</h1>
-          <p>Travelo is a travel agency that specializes in surprise vacations. Travelers fill out a 
-            survey about their travel preferences, budget etc.
-            </p>
+          <h1>TRAVEL TO EXPLORE</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+            natus, enim ipsam magnam odit deserunt itaque? Minima earum velit
+            tenetur!
+          </p>
         </div>
         <div className="search">
           <div className="container">
             <label htmlFor="">Where you want to go</label>
-            <input type="text" placeholder="Search Your Location" />
+            <input type="text" placeholder="Search Your location" />
           </div>
           <div className="container">
             <label htmlFor="">Check-in</label>
-            <input type="date"/>
+            <input type="date" />
           </div>
           <div className="container">
             <label htmlFor="">Check-out</label>
-            <input type="date"/>
+            <input type="date" />
           </div>
-          <button>
-            Explore Now
-          </button>
+          <button>Explore Now</button>
         </div>
       </div>
-
-      
     </Section>
-  )
+  );
 }
+
 const Section = styled.section`
-  position:relative;
-  margin-top:2rem;
-  width:100%;
-  height:100%;
-  .background{
-    img{
-      width:100%;
-      filter:brightness(40%);
+  position: relative;
+  margin-top: 2rem;
+  width: 100%;
+  height: 100%;
+
+  .background {
+    height: 100%;
+    img {
+      width: 100%;
+      filter: brightness(60%);
     }
   }
-  .content{
-    height:100%;
-    width:100%;
-    position:absolute;
-    top:0;
-    z-index:3;
-    text-align:center;
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    gap:1rem;
-    .title{
-      color:white;
-      h1{
-        font-size:3rem;
-        letter-spacing:0.2rem;
-
+  .content {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    z-index: 3;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    .title {
+      color: white;
+      h1 {
+        font-size: 3rem;
+        letter-spacing: 0.2rem;
       }
-      p{
-        color:white;
-        margin-top:0.5rem;
-        font-size:1.2rem;
-
-      }
-
-    }
-    .search{
-      display:flex;
-      justify-content: center;
-      background-color:#ffffffce;
-      padding:0.5rem;
-      border-radius:0.5rem;
-      .container{
-        display:flex;
-        align-items:center;
-        justify-content:center;
-        flex-direction:column;
-        padding:0 1.5rem;
-        label{
-          font-size:1.1rem;
-          color:#03045e;
-        }
-        input{
-          background-color:transparent;
-          border:none;
-          text-align:center;
-          color:black;
-          &[type="date"]{
-            padding-left:3rem;
-
-          }
-          &::placeholder{
-            color:black;
-          }
-          &:focus{
-            outline:none;
-
-          }
-        }
-      }
-      button{
-        padding:1rem;
-        cursor:pointer;
-        border-radius:0.3rem;
-        border:none;
-        color:white;
-        background-color:#4361ee;
-        font-size:1.1rem;
-        text-transform:uppercase;
-        transition:0.3s ease-in-out;
-        &:hover{
-          background-color:#023e8a;
-        }
-
+      p {
+        text-align: center;
+        padding: 0 30vw;
+        margin-top: 0.5rem;
+        font-size: 1.2rem;
       }
     }
-
-  }
-  @media Screen and (min-width: 280px) and (max-width: 1080px){
-    height:25rem;
-    .background{
-      img{
-        height:100%;
-
-      }
-    }
-    .content{
-      .title{
-        h1{
-          font-size:1rem;
-
+    .search {
+      display: flex;
+      background-color: #ffffffce;
+      padding: 0.5rem;
+      border-radius: 0.5rem;
+      .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        padding: 0 1.5rem;
+        label {
+          font-size: 1.1rem;
+          color: #03045e;
         }
-        p{
-          font-size:0.8rem;
-          padding:1vm;
+        input {
+          background-color: transparent;
+          border: none;
+          text-align: center;
+          color: black;
+          &[type="date"] {
+            padding-left: 3rem;
+          }
 
-        }
-      }
-      .search{
-        flex-direction:column;
-        padding:0.8rem;
-        gap:0.8rem;
-        .container{
-          padding:0 0.8rem;
-          input[type="date"]{
-            padding-left:1rem;
-
+          &::placeholder {
+            color: black;
+          }
+          &:focus {
+            outline: none;
           }
         }
-        button{
-          padding:1rem;
-          font-size:1rem;
+      }
+      button {
+        padding: 1rem;
+        cursor: pointer;
+        border-radius: 0.3rem;
+        border: none;
+        color: white;
+        background-color: #4361ee;
+        font-size: 1.1rem;
+        text-transform: uppercase;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          background-color: #023e8a;
         }
       }
     }
   }
-
+  @media screen and (min-width: 280px) and (max-width: 980px) {
+    height: 25rem;
+    .background {
+      background-color: palegreen;
+      img {
+        height: 100%;
+      }
+    }
+    .content {
+      .title {
+        h1 {
+          font-size: 1rem;
+        }
+        p {
+          font-size: 0.8rem;
+          padding: 1vw;
+        }
+      }
+      .search {
+        flex-direction: column;
+        padding: 0.8rem;
+        gap: 0.8rem;
+        /* padding: 0; */
+        .container {
+          padding: 0 0.8rem;
+          input[type="date"] {
+            padding-left: 1rem;
+          }
+        }
+        button {
+          padding: 1rem;
+          font-size: 1rem;
+        }
+        /* display: none; */
+      }
+    }
+  }
 `;
